ece9875b3ec8348c579e6c5eb943146865538b93
Aaryan Chauhan <aaryan.chauhan.1007@gmail.com>
2024-12-06 13:29:23 +0530

    Add files via upload

---
+++ Q524.py
@@ -0,0 +1,6 @@
+def fun(s):
+    print("upper" if s.isupper() else "not upper")    
+    print("lower" if s.islower() else "not lower")
+    print("upper: ",s.upper())
+    print("lower: ",s.lower())
+    
